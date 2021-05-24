ed9a121e02ec847f5317afbcd3cc6d08dd8219e0
PrashantRawat <prashant.rawat@tothenew.com>
2021-05-24 14:34:01 +0530

    HorizontalProgressView added

--- app/src/main/java/com/example/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/composerecipeapp/MainActivity.kt
@@ -2,11 +2,54 @@ package com.example.composerecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = null,
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Happy Meal", style = TextStyle(fontSize = 26.sp))
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(text = "800 Calories", style = TextStyle(fontSize = 17.sp))
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "$5.99",
+                        style = TextStyle(
+                            fontSize = 17.sp,
+                            color = Color(0xFF85bb65)
+                        )
+                    )
+                }
+            }
+        }
     }
 }
